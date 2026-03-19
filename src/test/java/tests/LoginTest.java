1de0c03db680cf18202f81f17ea63322a88b27cd
naveen18893-tech <naveen18893@gmail.com>
2026-03-19 16:05:06 +0530

    Update LoginTest.java

--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -1,103 +1,48 @@
-package tests;
+package base;
 
-import java.io.IOException;
+import java.time.Duration;
 
-import org.testng.Assert;
-import org.testng.annotations.DataProvider;
-import org.testng.annotations.Parameters;
-import org.testng.annotations.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 
-import base.BaseTest;
-import pages.LoginPage;
-import utils.ExcelUtils;
-import utils.ExtentReportManager;
-import utils.Log;
+import io.github.bonigarcia.wdm.WebDriverManager;
 
-public class LoginTest extends BaseTest {
-	
-	@DataProvider(name="LoginData")
-	public Object[][] getLoginData() throws IOException{
-		
-		String filePath = System.getProperty("user.dir")+"/testdata/TestData.xlsx";
-		ExcelUtils.loadExcel(filePath, "Sheet1");
-		int rowCount = ExcelUtils.getRowCount();
-		Object[][] data = new Object[rowCount-1][2];
-		
-		for(int i=1; i<rowCount; i++) {
-			
-			data[i-1][0] = ExcelUtils.getCellData(i, 0);	// Username
-			data[i-1][1] = ExcelUtils.getCellData(i, 1);	// Password
-		}
-		ExcelUtils.closeExcel();
-		return data;
-	}
-	
-	
-	@DataProvider(name="LoginData2")
-	public Object[][] getData(){
-		
-		return new Object[][] {
-			{"user1","pass1"},
-			{"user2","pass2"},
-			{"user3","pass3"}
-		};
-	}
-	
-	
+public class BaseTest {
 
-//	@Test(dataProvider = "LoginData2")
-//	@Test
-//	@Parameters({"username","password"})
-	@Test
-	public void testValidLogin() {
+    protected WebDriver driver;
 
-		Log.info("Starting login test...");
-		test = ExtentReportManager.createTest("Login Test - ");
+    @BeforeMethod
+    public void setUp() {
 
-		test.info("Navigating to URL");
-		LoginPage loginPage = new LoginPage(driver);
+        // Setup ChromeDriver automatically
+        WebDriverManager.chromedriver().setup();
 
-		Log.info("Adding credentials");
-		test.info("Adding Credentails");
-		loginPage.enterUsername("admin@yourstore.com");
-		loginPage.enterPassword("admin");
-//		loginPage.enterUsername(username);
-//		loginPage.enterPassword(password);
-		test.info("Clicking on Login button");
-		loginPage.clickLogin();
+        // Chrome options for Jenkins (headless execution)
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless=new");        // Run without UI
+        options.addArguments("--no-sandbox");          // Required for Jenkins/Linux
+        options.addArguments("--disable-dev-shm-usage"); // Fix memory issues
+        options.addArguments("--disable-gpu");
 
-		System.out.println("Title of the page is : " + driver.getTitle());
-		Log.info("Verifying page title");
-		test.info("Verifying page title");
-		Assert.assertEquals(driver.getTitle(), "Just a moment...");
+        // Initialize driver
+        driver = new ChromeDriver(options);
 
-		test.pass("Login Successful");
+        // Common settings
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.manage().window().maximize();
 
-	}
+        // Open your application URL
+        driver.get("https://admin-demo.nopcommerce.com/login");
+    }
 
-//	@Test
-//	public void testLoginWithInvalidCredentials() {
-//
-//		Log.info("Starting login test...");
-//		test = ExtentReportManager.createTest("Login Test with Invalid Credentials");
-//
-//		test.info("Navigating to URL");
-//		LoginPage loginPage = new LoginPage(driver);
-//
-//		Log.info("Adding credentials");
-//		test.info("Adding Credentails");
-//		loginPage.enterUsername("admin1234@yourstore.com");
-//		loginPage.enterPassword("admin123");
-//		test.info("Clicking on Login button");
-//		loginPage.clickLogin();
-//
-//		System.out.println("Title of the page is : " + driver.getTitle());
-//		Log.info("Verifying page title");
-//		test.info("Verifying page title");
-//		Assert.assertEquals(driver.getTitle(), "Just a moment...123");
-//
-//		test.pass("Login Successful");
-//
-//	}
+    @AfterMethod
+    public void tearDown() {
 
+        if (driver != null) {
+            driver.quit();
+        }
+    }
 }

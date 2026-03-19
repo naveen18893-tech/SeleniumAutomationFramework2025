c8094b6fcc3e512783c76fe53e469c9b3d47f818
naveen18893-tech <naveen18893@gmail.com>
2026-03-19 16:07:52 +0530

    Update Test.java

--- src/test/java/tests/Test.java
+++ src/test/java/tests/Test.java
@@ -1,5 +1,43 @@
-package tests;
+package base;
 
-public class Test{
-	
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+public class BaseTest {
+
+    protected WebDriver driver;
+
+    @BeforeMethod
+    public void setUp() {
+
+        // Chrome options for Jenkins (headless mode)
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless=new");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--disable-gpu");
+
+        // Initialize driver
+        driver = new ChromeDriver(options);
+
+        // Common setup
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.manage().window().maximize();
+
+        // Launch application
+        driver.get("https://admin-demo.nopcommerce.com/login");
+    }
+
+    @AfterMethod
+    public void tearDown() {
+
+        if (driver != null) {
+            driver.quit();
+        }
+    }
 }
